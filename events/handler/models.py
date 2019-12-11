77b7e792e1ac1ec34656e307ade4cc2ef7f1b48f
Sergey Alexeev <demiurg58@gmail.com>
2019-12-11 14:34:33 +0300

    add events and tag models, migration, admin

--- events/handler/models.py
+++ events/handler/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=512, db_index=True)
+    place = models.ForeignKey('Place', on_delete=models.CASCADE)
+    event_time = models.DateTimeField()
+    body = models.TextField()
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
+
+
+class Place(models.Model):
+    pass
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.title
