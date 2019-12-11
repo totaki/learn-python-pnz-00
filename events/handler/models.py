d01476218e52dd270f8e1a75a9ba5fee830499a7
Artem Kuznetsov <759167@mail.ru>
2019-12-11 18:05:00 +0300

    Change house number

--- events/handler/models.py
+++ events/handler/models.py
@@ -5,8 +5,8 @@ class Place(models.Model):
     place_name = models.CharField(max_length=512)
     city = models.CharField(max_length=32)
     street = models.CharField(max_length=32)
-    house_number = models.IntegerField()
-    office_number = models.IntegerField()
+    house_number = models.CharField(max_length=16)
+    office_number = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.place_name
