839ca05730c85d2d0cebae70f7ad3abfb1c32508
Artem Kuznetsov <759167@mail.ru>
2019-12-21 20:47:15 +0300

    Fixed exists and get_place

--- events/scheduler/save_event.py
+++ events/scheduler/save_event.py
@@ -5,32 +5,35 @@ from typing import List
 logger = logging.getLogger('__name__')
 
 
+def get_place(event):
+    try:
+        place = Place.objects.get(place_name=event['place_name'])
+    except Place.DoesNotExist:
+        place = Place.objects.create(
+            place_name=event['place_name'],
+            city=event['city'],
+            street=event['street'],
+            house_number=event['house_number'],
+            office_number=event['office_number']
+        )
+    return place
+
+
 def save_event(events: List) -> List:
     id_new_events = []
     for event in events:
+        place = get_place(event)
         try:
-            place = Place.objects.get(place_name=event['place_name'])
-        except Place.DoesNotExist:
-            Place.objects.create(
-                place_name=event['place_name'],
-                city=event['city'],
-                street=event['street'],
-                house_number=event['house_number'],
-                office_number=event['office_number']
-            )
-            place = Place.objects.get(place_name=event['place_name'])
-        db_events = Event.objects.filter(title=event['title'], event_time=event['event_time'])
-        if not db_events:
-            Event.objects.create(
+            Event.objects.get(title=event['title'], event_time=event['event_time'])
+            print(f"The event {event['title']} is in the database")
+        except Event.DoesNotExist:
+            new_event = Event.objects.create(
                 title=event['title'],
                 place=place,
                 event_time=event['event_time'],
                 body=event['body']
             )
-            id_new = Event.objects.last().id
-            id_new_events.append(id_new)
-        else:
-            logger.info("The event is in the database")
+            id_new_events.append(new_event.id)
     return id_new_events
 
 
