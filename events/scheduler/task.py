03ca9c34279e949e5bc80524f801d93cac3e7bc1
SuperSerg58 <demiurg58@gmail.com>
2019-12-21 13:47:05 +0300

    add BaseTask

---
+++ events/scheduler/task.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+
+class Task:
+    def __init__(self, timeout, runner, callback):
+        self.timeout = timeout  # в секундах
+        self.runner = runner  # Сама функция, которая будет запускаться, в нашем случае Fetcher
+        self.callback = callback  # Сюда мы будем передавать результат, сохранить ивенты
+        self.next = datetime.utcnow()
+
+    @property
+    def need_run(self, datetime):
+        """
+        тут надо реализовать проверку того что мы должны запустить данные таск,
+        в нашем случае просто проверяем что self.next меньше времени, которое мы передали
+        """
+        if self.next < datetime:
+            return True
+
+    def update_next(self):
+        """
+        Тут обновляем наш next
+        """
+        self.next += self.timeout
