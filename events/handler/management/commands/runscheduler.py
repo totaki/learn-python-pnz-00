43bf13a7ae48b48c0e600c2515af3459612c94e8
Artem Kuznetsov <759167@mail.ru>
2019-12-23 20:30:19 +0300

    Change type add arg

--- events/handler/management/commands/runscheduler.py
+++ events/handler/management/commands/runscheduler.py
@@ -1,17 +1,17 @@
 from django.core.management.base import BaseCommand
 from scheduler.scheduler import Scheduler
 from fetcher.fetcher import Fetcher
-from typing import List
+
 
 TASKS_MAP = {
-    'fetcher': Fetcher()
+    'tasks': [Fetcher(), ]
 }
 
 
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument('tasks', type=List)
+        parser.add_argument('tasks', type=str)
         parser.add_argument('timeout', type=int)
 
     def handle(self, *args, **options):
