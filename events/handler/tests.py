59a094d6246c597c47fdfc1187dfc0d539382e09
Artem Kuznetsov <759167@mail.ru>
2019-12-23 13:03:55 +0300

    Added test save_event

--- events/handler/tests.py
+++ events/handler/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from handler.models import Event, Place
+from scheduler.save_event import save_event
 
-# Create your tests here.
+
+class SaveEventTestCase(TestCase):
+    def setUp(self):
+        event = [{
+            'title': "Король Вечного Сна",
+            'body': "Трибьют группы Король и Шут",
+            'event_time': "2020-01-06 19:00:00",
+            'place_name': "FrauGross",
+            'city': "Пенза",
+            'street': "Пр-кт Строителей",
+            'house_number': "152 Б",
+            'office_number': 4
+        }]
+        save_event(event)
+
+    def test_save_event(self):
+        """Events"""
+
+        event = Event.objects.get(title='Король Вечного Сна')
+        place = Place.objects.get(place_name='FrauGross')
+        self.assertEqual(event.body, 'Трибьют группы Король и Шут')
+        self.assertEqual(place.street, 'Пр-кт Строителей')
