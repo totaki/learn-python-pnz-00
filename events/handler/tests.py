29de1c124c576e497ba8ffc5443c1dc7e6207528
Artem Kuznetsov <759167@mail.ru>
2019-12-23 19:10:47 +0300

    Added 2th test save_event

--- events/handler/tests.py
+++ events/handler/tests.py
@@ -5,7 +5,7 @@ from scheduler.save_event import save_event
 
 class SaveEventTestCase(TestCase):
     def setUp(self):
-        event = [{
+        self.events = [{
             'title': "Король Вечного Сна",
             'body': "Трибьют группы Король и Шут",
             'event_time': "2020-01-06 19:00:00",
@@ -15,12 +15,18 @@ class SaveEventTestCase(TestCase):
             'house_number': "152 Б",
             'office_number': 4
         }]
-        save_event(event)
+        save_event(self.events)
 
     def test_save_event(self):
-        """Events"""
+        """Тест записи в базу функцией save_event и чтения записанных данных из базы"""
 
         event = Event.objects.get(title='Король Вечного Сна')
         place = Place.objects.get(place_name='FrauGross')
         self.assertEqual(event.body, 'Трибьют группы Король и Шут')
         self.assertEqual(place.street, 'Пр-кт Строителей')
+
+    def test_dont_save_duplicate_event(self):
+        """Тест проверки на дублирование записи. При попытке записи имеющегося в базе события,
+        функция save_event возвращает пустой список."""
+
+        self.assertEqual(save_event(self.events), [])
