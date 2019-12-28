40ada7d5bed1ccb80f55d97ee28bfe2352fc0ae8
Artem Kuznetsov <759167@mail.ru>
2019-12-28 17:34:47 +0300

    Add command tag and create notificate in bot

--- events/handler/event_bot.py
+++ events/handler/event_bot.py
@@ -1,11 +1,12 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from handler.models import User
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler
+from handler.models import User, Tag, Notificaton
+from telegram import ReplyKeyboardMarkup
 
 logger = logging.getLogger(__name__)
 
 
-def start(update, context):
+def start(update):
     """Save user in database and send a message when the command /start is issued."""
     user_id = update.message.chat.id
     username = update.message.chat.username
@@ -19,6 +20,29 @@ def start(update, context):
         update.message.reply_text(f'Hi, {username}! Your id {user_id}. Now is {message_date}')
 
 
+def tags(update):
+    """Print tags"""
+    user_id = update.message.chat.id
+    db_id = User.objects.get(external_id=user_id)
+    if user_id in db_id:
+        concert_tags = ['Rock', 'Pop', 'Game', 'Classic']
+        tag_keyboard = ReplyKeyboardMarkup([concert_tags])
+        update.message.reply_text('Choose tag', reply_markup=tag_keyboard)
+    else:
+        update.message.reply_text('Use command: /start, to start working with bot')
+
+
+def create_notifications(update, user_data):
+    """Create notification in database"""
+    chat_id = update.message.chat.id
+    Notification.objects.create(tag=user_data[0], chat_id=chat_id)
+    update.message.reply_text(f'You are subscribed to {user_data}')
+
+
+def send_notifications(chat_id, message):
+    pass
+
+
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -29,6 +53,12 @@ def event_bot(token, PROXY):
     updater = Updater(token, request_kwargs=PROXY, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("tags", tags))
+    dp.add_handler(
+        RegexHandler('^()$',
+                     send_cat,
+                     pass_user_data=True)
+    )
     dp.add_error_handler(error)
     updater.start_polling()
     updater.idle()
