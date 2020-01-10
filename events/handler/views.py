11d28a5d916f056b0a1827ca888214c2818c5f8b
Artem Kuznetsov <759167@mail.ru>
2020-01-10 15:16:46 +0300

    Start works for web

--- events/handler/views.py
+++ events/handler/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from handler.models import Event, Place
+from datetime import datetime, timedelta
 
-# Create your views here.
+
+def index(request):
+    start_date = datetime.utcnow()
+    end_date = start_date + timedelta(days=365)
+    event_list = Event.objects.filter(event_time__range=(start_date, end_date))
+    context = {'event_list': event_list}
+    return render(request, 'handler/index.html', context)
+
+
+def place(request):
+    place_list = Place.objects.all()
+    context = {'place_list': place_list}
+    return render(request, 'handler/place.html', context)
